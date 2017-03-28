f3dcc4b9487baeabf24c1b5c7b3a8683a1a18b1a
datoneace <ah308508@students.davenport.k12.ia.us>
2017-03-28 12:51:54 -0500

    update

---
+++ Chapter1/src/caseProblems/carlysMotto.java
@@ -0,0 +1,14 @@
+package caseProblems;
+
+public class carlysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("************************************************");
+		System.out.println("**Carly's makes the food that makes it a party**");
+		System.out.println("**       Sammy's makes it fun in the sun      **");
+		System.out.println("************************************************");
+		
+	}
+
+}
---
+++ FirstProject/src/hello/helloworld.java
@@ -0,0 +1,25 @@
+package hello;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		//Everything begins  with the main
+		//Write a program that says hello world
+		
+		System.out.println("hello world");
+		System.out.println("Look mom Im Programming");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("***************");
+		System.out.println("*             *");
+		System.out.println("*             *");
+		System.out.println("*             *");
+		System.out.println("*             *");
+		System.out.println("*             *");
+	}
+
+} 
