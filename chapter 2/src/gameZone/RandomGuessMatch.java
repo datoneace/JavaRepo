ad8d11412bb8d5a3191297a8f0ace23f7e21c4a6
datoneace <ah308508@students.davenport.k12.ia.us>
2017-04-05 12:21:09 -0500

    changes

---
+++ chapter 2/src/gameZone/RandomGuessMatch.java
@@ -0,0 +1,38 @@
+package gameZone;
+
+import javax.swing.JOptionPane;
+
+public class RandomGuessMatch {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		displayOutcome();
+		MadLibs.secondGame();
+
+		
+	}
+		public static void displayOutcome() 
+	{	
+		final int MIN = 1;
+		final int MAX = 5;
+		int random;
+		int guess;
+		boolean isMatch;
+		
+		String guessString;
+		random = MIN + (int)(Math.random() * MAX);
+		
+		guessString = JOptionPane.showInputDialog(null, "Guess my number between " + MIN + "and " +
+		MAX, "Random guess", JOptionPane.INFORMATION_MESSAGE);
+		
+		guess = Integer.parseInt(guessString);
+		isMatch = guess == random; 
+		
+		
+		JOptionPane.showMessageDialog(null, "Your guess was " + (random-guess) + " number away from the random number.");
+		JOptionPane.showMessageDialog(null, "Your number was " + random + ". The result is " + isMatch + ".");
+	}
+		
+}
