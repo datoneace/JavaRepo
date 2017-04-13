1a1dd0eff371db0f6fee2e2f3fa653c8f3f35a5c
datoneace <ah308508@students.davenport.k12.ia.us>
2017-04-13 12:45:43 -0500

    changes

---
+++ Chapter 3/src/Debugging/debuggingThree4.java
@@ -0,0 +1,25 @@
+package Debugging;
+
+public class debuggingThree4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myCredits = 13;
+	      int yourCredits = 17;
+	      double rate = 75.84;
+	      System.out.println("My tuition:");
+	      tuitionBill(myCredits, rate);
+	      System.out.println("Your tuition:");
+	      tuitionBill(yourCredits, rate);
+	    
+	      
+	   }
+	   
+	public static void tuitionBill(int c, double r)
+	   {
+	   System.out.println("Total due " + (c *r));
+	   System.out.println("Total due " + (c *r)); 
+	}
+	}
+
+
